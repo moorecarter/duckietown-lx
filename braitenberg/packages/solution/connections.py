608c341332e2da5a3d4866ea2b90aec473ba272a
moorecarter <moorecarter15@gmail.com>
2024-02-26 20:34:19 -0500

    pushing braitenberg final

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -8,12 +8,23 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
     #(480, 640)
-    for i in range(0, 9):
-        res[300:, 32*i:32*(i+1)] = -0.0625 * (i-10)
-    for i in range(10, 20):
-        res[300:, 32*i:32*(i+1)] = -0.0625 * (i-10)
-            
     
+    for i in range(0, 80):
+        res[140+(2*i):480, 0:320] = 0.0125 * i
+    for i in range(0, 80):
+        res[200-i:, i*4:4*(i+4)] += -0.0015*(i-80)
+
+    for i in range(0, 80):
+        res[140+(2*i):480, 320:] = -0.0125 * i
+    for i in range(0, 80):
+        res[120+i:, i*4+320:4*(i+4)+320] += 0.0015*(-i)
+
+    for i in range(0, 80):
+        res[0:200-i, i*4:4*(i+1)] = -0.35
+    for i in range(0, 80):
+        res[0:120+i, i*4+320:4*(i+1)+320] = 0.35
+        
+
     # ---
     return res
 
@@ -22,9 +33,20 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    for i in range(0, 9):
-        res[300:, 32*i:32*(i+1)] = 0.0625 * (i-10)
-    for i in range(10, 20):
-        res[300:, 32*i:32*(i+1)] = 0.0625 * (i-10)
+    for i in range(0, 80):
+        res[140+(2*i):480, 0:320] = -0.0125 * i
+    for i in range(0, 80):
+        res[200-i:, i*4:4*(i+4)] += 0.0015*(i-80)
+
+    for i in range(0, 80):
+        res[140+(2*i):480, 320:] = 0.0125 * i
+    for i in range(0, 80):
+        res[120+i:, i*4+320:4*(i+4)+320] += -0.0015*(-i)
+
+    for i in range(0, 80):
+        res[0:200-i, i*4:4*(i+1)] = 0.35
+    for i in range(0, 80):
+        res[0:120+i, i*4+320:4*(i+1)+320] = -0.35
+        
     # ---
     return res
